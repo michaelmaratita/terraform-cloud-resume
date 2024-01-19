530c43fab6fb2a691c83863a6f50f0ded6e5df27
Michael Maratita <mikool671@gmail.com>
2024-01-19 10:17:12 -0800

    modified resource-s3.tf to remove bucket_policy

--- infra/modules/resource-s3.tf
+++ infra/modules/resource-s3.tf
@@ -45,24 +45,3 @@ resource "aws_s3_object" "upload_html" {
   content_type           = "text/html"
   etag                 = filemd5("${var.public_path}/html_files/${each.key}")
 }
-
-resource "aws_s3_bucket_policy" "bucket_policy" {
-  bucket = aws_s3_bucket.cdn_bucket.bucket
-  policy = jsonencode({
-    "Version" = "2012-10-17",
-    "Statement" = {
-            "Sid" = "AllowCloudFrontServicePrincipalReadOnly",
-            "Effect" = "Allow",
-            "Principal" = {
-                "Service" = "cloudfront.amazonaws.com"
-            },
-            "Action" = "s3:GetObject",
-            "Resource" = "arn:aws:s3:::${aws_s3_bucket.cdn_bucket.id}/*",
-            "Condition" = {
-                "StringEquals" = {
-                   "AWS:SourceArn" = "arn:aws:cloudfront::${data.aws_caller_identity.current.account_id}:distribution/${aws_cloudfront_distribution.s3_distribution.id}"
-                }
-            }
-        }
-  })
-}
