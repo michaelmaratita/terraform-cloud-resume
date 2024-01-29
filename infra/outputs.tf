5c257e2ca11c56278be9edf8e9581e548a138f67
Michael Maratita <mikool671@gmail.com>
2024-01-29 10:54:52 -0800

    fixed outputs to include distribution ID for GitHub actions CloudFront Invalidation

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -2,3 +2,8 @@ output "url" {
   description = "my resume site url"
   value = module.resume_site.url
 }
+
+output "cloudfront_distribution_id" {
+  description = "Output for CloudFront distribution ID for GitHub actions"
+  value = module.resume_site.cloudfront_distribution_id
+}
