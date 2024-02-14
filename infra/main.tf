808a82d11b1d35b99b9eaeb1991045bb48f43d79
Michael Maratita <mikool671@gmail.com>
2024-02-13 23:54:06 -0800

    modified source to portfolio website folder

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,5 @@
 module "resume_site" {
-  source = "github.com/michaelmaratita/Terraform-Modules"
+  source = "github.com/michaelmaratita/Terraform-Modules/modules/portfolio_website"
 
   # AWS Certificate Manager (ACM)
   my_domain                 = var.my_domain
